91cab27d5ef4f619e37ecfb84cae36b8311b4e50
Crois0509 <crois0509@naver.com>
2025-01-07 18:38:24 +0900

    [Feat] 코어데이터 프로토콜 추가

--- OneMoreMinute/OneMoreMinute/Source/RepositoryManager/CoreDateProtocol.swift
+++ OneMoreMinute/OneMoreMinute/Source/RepositoryManager/CoreDateProtocol.swift
@@ -5,4 +5,30 @@
 //  Created by 장상경 on 1/7/25.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+protocol CoreDataManaged: AnyObject {
+    associatedtype Model: EntityTransformAble
+    associatedtype Entity: NSManagedObject
+    
+    var persistentContainer: NSPersistentContainer { get }
+    
+    func create()
+    func fetch()
+    func delete()
+}
+
+protocol EntityTransformAble {
+    associatedtype Entity: NSManagedObject
+    func toEntity(context: NSManagedObjectContext) -> Entity
+}
+
+extension CoreDataManaged {
+    var persistentContainer: NSPersistentContainer {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return NSPersistentContainer() }
+        let container = appDelegate.persistentContainer
+        
+        return container
+    }
+}
