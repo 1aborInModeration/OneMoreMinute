9724fb3408a554b67d9cc003cdf788831a438053
Crois0509 <crois0509@naver.com>
2025-01-07 20:53:41 +0900

    [Feat] 알람 엔티티 DTO 파일 생성

--- OneMoreMinute/OneMoreMinute/Source/RepositoryManager/CoreDataManager.swift
+++ OneMoreMinute/OneMoreMinute/Source/RepositoryManager/CoreDataManager.swift
@@ -5,4 +5,38 @@
 //  Created by 장상경 on 1/7/25.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+final class CoreDataManager: CoreDataManaged {
+    typealias Model = AlarmDTO
+    typealias Entity = Alarm
+    
+    private lazy var context = self.persistentContainer.viewContext
+    
+    static let shared = CoreDataManager()
+    private init() {}
+
+    func create(with model: Model) {
+        _ = model.toEntity(context: context)
+        save()
+    }
+    
+    func fetch() -> [Entity] {
+        let request: NSFetchRequest<Entity> = Entity.fetchRequest()
+        return (try? context.fetch(request)) ?? []
+    }
+    
+    func delete(_ entity: Entity) {
+        context.delete(entity)
+        save()
+    }
+    
+    func save() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Error saving Core Data: \(error)")
+        }
+    }
+}
