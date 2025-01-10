fdaeed96fe95cf52f50ddedc549f5b6750e903dd
Crois0509 <crois0509@naver.com>
2025-01-10 20:03:15 +0900

    [Feat] 세계시간 데이터 관리 객체 생성

--- OneMoreMinute/OneMoreMinute/Source/RepositoryManager/WorldTimeDataManager/WorldTimeDTO.swift
+++ OneMoreMinute/OneMoreMinute/Source/RepositoryManager/WorldTimeDataManager/WorldTimeDTO.swift
@@ -8,11 +8,15 @@
 import UIKit
 import CoreData
 
+/// WorldTime 엔티티의 데이터 파일
 struct WorldTimeDTO: EntityTransformAble {
     let cityName: String
     let timeZoneld: String
     let createdAt: Date
     
+    /// WorldTime 엔티티 데이터 타입을 반환하는 메소드
+    /// - Parameter context: 엔티티 관리 Context
+    /// - Returns: WorldTime 엔티티 데이터 타입
     func toEntity(context: NSManagedObjectContext) -> WorldTimeEntity {
         let wordTimeEntity = WorldTimeEntity(context: context)
         wordTimeEntity.cityName = self.cityName
--- OneMoreMinute/OneMoreMinute/Source/RepositoryManager/WorldTimeDataManager/WorldTimeDataManager.swift
+++ OneMoreMinute/OneMoreMinute/Source/RepositoryManager/WorldTimeDataManager/WorldTimeDataManager.swift
@@ -5,4 +5,77 @@
 //  Created by 장상경 on 1/10/25.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+/// WorldTime의 엔티티를 관리하는 코어 데이터 객체
+final class WorldTimeDataManager: CoreDataManaged {
+    typealias Model = WorldTimeDTO
+    typealias Entity = WorldTimeEntity
+    
+    private lazy var context = self.persistentContainer.viewContext
+    
+    static let shared = WorldTimeDataManager() // 싱글톤
+    private init() {}
+    
+    // MARK: - AlarmDataManager CRUD Method
+    
+    /// Alarm 엔티티에 새로운 데이터를 추가하는 메소드
+    /// - Parameter model: AlarmDTO
+    ///
+    /// ``EntityTransformAble``
+    /// ``AlarmDTO``
+    func create(with model: Model) {
+        _ = model.toEntity(context: context)
+        try? save()
+    }
+    
+    /// Alarm 엔티티의 데이터 목록을 불러오는 메소드
+    /// - Returns: Alarm 엔티티의 데이터 목록 배열
+    func fetch() -> [Entity] {
+        let request: NSFetchRequest<Entity> = Entity.fetchRequest()
+        return (try? context.fetch(request)) ?? []
+    }
+    
+    /// 특정 엔티티를 업데이트 시키는 메소드
+    /// - Parameters:
+    ///   - id: 업데이트를 원하는 데이터의 ID
+    ///   - updateData: 업데이트 데이터
+    func update(_ id: NSManagedObjectID, updateData: Entity) {
+        guard var _ = search(id) else { return }
+        _ = updateData
+        try? save()
+    }
+    
+    /// 특정 엔티티 데이터를 찾는 메소드
+    /// - Parameter id: 찾고싶은 데이터의 ID
+    /// - Returns: 특정 엔티티 데이터
+    func search(_ id: NSManagedObjectID) -> Entity? {
+        do {
+            let data = try context.existingObject(with: id) as? Entity
+            return data
+            
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    /// Alarm 엔티티의 데이터를 삭제하는 메소드
+    /// - Parameter entity: 삭제할 Alarm 데이터
+    func delete(_ entity: Entity) {
+        context.delete(entity)
+        try? save()
+    }
+    
+    /// Alarm 엔티티의 변경사항을 저장하는 메소드
+    func save() throws {
+        guard context.hasChanges else { return }
+        
+        do {
+            try context.save()
+        } catch let error {
+            print("Error saving Core Data: \(error)")
+        }
+    }
+}
