91c6b85a7ef7701d64d8960891e899d2fba5c0db
ericKwon95 <ericyong95@gmail.com>
2025-01-14 17:55:24 +0900

    [Feat] 더미 VC 제거, 알람 끄기 모달 보이기 및 제거하기 로직 구현

--- OneMoreMinute/OneMoreMinute/Source/View/Tab/MainTabBarController.swift
+++ OneMoreMinute/OneMoreMinute/Source/View/Tab/MainTabBarController.swift
@@ -171,75 +171,24 @@ extension MainTabBarController {
     }
 }
 
-// TODO: 각 임시 뷰컨 실제 뷰컨으로 변경하기
+// MARK: - Modal Presentation
 
-/// 임시 뷰 컨트롤러 A - 카운터 기능을 가진 테스트용 뷰 컨트롤러
-final class AViewController: UIViewController {
-    private let alarmManager = AlarmManager()
-    private lazy var playButton = UIButton().then {
-        $0.setTitle("play", for: .normal)
-        $0.addAction(
-            UIAction { _ in
-                do {
-                    try self.alarmManager.playAlarm(sound: .morning, numberOfLoops: 0)
-                } catch {
-                    print(error)
-                }
-            },
-            for: .touchUpInside
-        )
+extension MainTabBarController {
+    private func showSnoozeModal(time: String, title: String) {
+        let snoozeVC = AlarmSnoozeHostingViewController(time: time, title: title) {
+            self.dismissSnoozeModal()
+        }
+        snoozeVC.modalPresentationStyle = .overFullScreen
+        snoozeVC.modalTransitionStyle = .crossDissolve
+        present(snoozeVC, animated: false)
     }
     
-    private lazy var stopButton = UIButton().then {
-        $0.setTitle("stop", for: .normal)
-        $0.addAction(
-            UIAction { _ in
-                do {
-                    try self.alarmManager.stopAlarm()
-                } catch {
-                    print(error)
-                }
-            },
-            for: .touchUpInside
-        )
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        [
-            playButton,
-            stopButton
-        ].forEach { view.addSubview($0) }
-        
-        playButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-        
-        stopButton.snp.makeConstraints { make in
-            make.top.equalTo(playButton.snp.bottom).offset(16)
-            make.centerX.equalToSuperview()
-        }
+    private func dismissSnoozeModal() {
+        self.presentedViewController?.dismiss(animated: false)
     }
 }
 
-final class DViewController: UIViewController {
-    private let label = UILabel().then {
-        $0.text = "D"
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        [
-            label,
-        ].forEach { view.addSubview($0) }
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-    }
+@available(iOS 17.0, *)
+#Preview {
+    MainTabBarController()
 }
-
-//@available(iOS 17.0, *)
-//#Preview {
-//    MainTabBarController()
-//}
