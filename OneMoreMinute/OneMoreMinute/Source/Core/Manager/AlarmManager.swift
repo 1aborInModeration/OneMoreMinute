6ca65063be5094392796a2b5cbc33b8c78ac21bc
ericKwon95 <ericyong95@gmail.com>
2025-01-14 15:16:14 +0900

    [Docs] 주석 작성

--- OneMoreMinute/OneMoreMinute/Source/Core/Manager/AlarmManager.swift
+++ OneMoreMinute/OneMoreMinute/Source/Core/Manager/AlarmManager.swift
@@ -13,10 +13,12 @@ protocol AlarmManageable {
     func stopAlarm() throws
 }
 
+/// 알람 재생 중 발생하는 에러 열거형
 enum AlarmError: LocalizedError {
     case invalidFileUrl
 }
 
+/// 재생할 알람 소리 종류를 나타내는 열거형
 enum AlarmSound {
     case morning
     
@@ -28,9 +30,14 @@ enum AlarmSound {
     }
 }
 
+/// 앱에서 알람 소리를 재생하는 기능을 가지는 객체
 final class AlarmManager: AlarmManageable {
     private var player: AVAudioPlayer?
     
+    /// 지정된 알람 소리를 재생하는 함수
+    /// - Parameters:
+    ///   - sound: 재생할 알람 소리 -> AlarmSound 열거형 참고
+    ///   - numberOfLoops: 몇 번 재생할 것인지 설정하는 매개변수
     func playAlarm(sound: AlarmSound, numberOfLoops: Int) throws {
         guard !(player?.isPlaying ?? false) else {
             return
@@ -46,6 +53,7 @@ final class AlarmManager: AlarmManageable {
         player?.play()
     }
     
+    /// 알람 소리 재생 멈춤 함수
     func stopAlarm() throws {
         player?.stop()
     }
